df5efb9a95ce989efc90a793486e71611110373b
Anzury <worstjgl@gmail.com>
2022-11-24 11:48:44 +0100

    deleted:    bin/App.class 	new file:   bin/Baseball.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,426 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("mange mon gros cul!");
+Raclette
+#2000
+
+Juanfer MERCIER — 22/11/2022 17:27
+on est bien d'accord que le prof a demandé à la question 1 "Comment peut-on" et pas "Montrer que c'est pas bon" ?
+Raclette — 22/11/2022 17:29
+ouep
+mais c'set comment peut monter CETTE erreur 
+Juanfer MERCIER — 22/11/2022 17:30
+ouais mais genre "comment" pas genre "faites-le"😂
+donc moi j'ai montré mais j'ai pas fait
+Raclette — 22/11/2022 17:31
+xd
+Juanfer MERCIER — 22/11/2022 17:31
+Question mal posée wallah
+Raclette — 22/11/2022 17:31
+whallah donne els points
+Juanfer MERCIER — 22/11/2022 17:32
+de ouf😂c'est pas possible wsh
+Je vais envoyé qqun lui dire "mais monsieur la question c'était comment faire pas de le faire du coup Juan c'est un génie incompris wallah"
+Raclette — 22/11/2022 17:53
+le pauvre
+Raclette — 22/11/2022 19:37
+tu as le cc de l'année dernière en opti nn linéaire??
+Juanfer MERCIER — 22/11/2022 19:38
+nope, il a écrit sur le tableau il me semble
+Raclette — 22/11/2022 19:38
+ptdr
+Raclette — 22/11/2022 19:45
+irena m'a dit que c'est mieux que tu passe au groupe 2 pr les tp parce que y a vla moin de monde en groupe 2
+Juanfer MERCIER — 22/11/2022 19:46
+groupe 2 c'est pas le matin?
+Raclette — 22/11/2022 19:46
+si
+mais du coup t'as r mercredi
+Juanfer MERCIER — 22/11/2022 19:47
+ptn jvais morfler🥲
+Juanfer MERCIER — 22/11/2022 19:47
+hummm vu comme ça c'est pas mal
+Raclette — 22/11/2022 19:47
+xd
+Raclette — Hier à 09:51
+Tu viens à quelle heure sur le campus?
+Si tu viens xd
+Juanfer MERCIER — Hier à 14:17
+j'étais censé être là à 11h
+censé
+là jsuis en chemin😅
+Raclette — Hier à 14:23
+Ptdr je suis rentré depuis un moment du coup
+Juanfer MERCIER — Hier à 14:24
+Ouais j'imagine, faudra que jte passe mon num parce que les notifs discord n'apparaissent pas tout le temps sur discord
+Juanfer MERCIER — Hier à 14:24
+j'y vais pour confirmer le changement de groupe avec la dame et chopper le sujet
+Juanfer MERCIER — Hier à 14:54
+Type de fichier joint : acrobat
+BaseballEliminationWithoutN.pdf
+74.51 KB
+Juanfer MERCIER — Hier à 15:12
+Type de fichier joint : acrobat
+lecture2.pdf
+125.10 KB
+https://tobykingsman.wordpress.com/2016/04/15/baseball-elimination-problem/
+Toby Kingsman
+Toby Kingsman
+Baseball Elimination Problem
+Recently we had a masterclass from Pitu Mirchandani on the subject of network flows. These network flow problems have been developed since the 1950’s an example of which is my previous post o…
+Baseball Elimination Problem
+on a déjà quelques réponses en lisant les ressources sur lesquelles elle s'est inspirée
+Juanfer MERCIER — Hier à 19:38
+ça te dérange si jte laisse le TP de graphes pour l'instant?? Comment ça je taff l'ACO pendant l'heure
+Raclette — Hier à 19:39
+ouais on peut faire comme ça et puis ce weekend on fait a balle ACO
+Juanfer MERCIER — Hier à 19:41
+let's go, duo de shock
+jvais mettre en place le git déjà
+Juanfer MERCIER — Hier à 20:17
+jt'ai envoyer l'invit
+Juanfer MERCIER — Aujourd’hui à 09:03
+neovim qui décide de plus fonctionner🥲pète les couilles, l'ACO va pas se faire tout seul mdr
+Raclette — Aujourd’hui à 09:04
+:Madge:
+Juanfer MERCIER — Aujourd’hui à 09:24
+t'es au campus déjà?
+Raclette — Aujourd’hui à 09:29
+yep je suis dans la salle
+Juanfer MERCIER — Aujourd’hui à 10:48
+t'as quelle version de gcc? test avec 
+gcc -v
+Raclette — Aujourd’hui à 10:49
+11.2
+gcc version 11.2.0 (Ubuntu 11.2.0-19ubuntu1)
+Juanfer MERCIER — Aujourd’hui à 10:49
+lance 
+cpp -v /dev/null -o /dev/null
+ pour voir si t'as les includes qu'il me manque
+et envoie-moi le résultat
+Raclette — Aujourd’hui à 10:50
+Using built-in specs.
+COLLECT_GCC=cpp
+OFFLOAD_TARGET_NAMES=nvptx-none:amdgcn-amdhsa
+OFFLOAD_TARGET_DEFAULT=1
+Target: x86_64-linux-gnu
+Configured with: ../src/configure -v --with-pkgversion='Ubuntu 11.2.0-19ubuntu1' --with-bugurl=file:///usr/share/doc/gcc-11/README.Bugs --enable-languages=c,ada,c++,go,brig,d,fortran,objc,obj-c++,m2 --prefix=/usr --with-gcc-major-version-only --program-suffix=-11 --program-prefix=x86_64-linux-gnu- --enable-shared --enable-linker-build-id --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --libdir=/usr/lib --enable-nls --enable-bootstrap --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-vtable-verify --enable-plugin --enable-default-pie --with-system-zlib --enable-libphobos-checking=release --with-target-system-zlib=auto --enable-objc-gc=auto --enable-multiarch --disable-werror --enable-cet --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --enable-multilib --with-tune=generic --enable-offload-targets=nvptx-none=/build/gcc-11-gBFGDP/gcc-11-11.2.0/debian/tmp-nvptx/usr,amdgcn-amdhsa=/build/gcc-11-gBFGDP/gcc-11-11.2.0/debian/tmp-gcn/usr --without-cuda-driver --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --with-build-config=bootstrap-lto-lean --enable-link-serialization=2
+Thread model: posix
+Supported LTO compression algorithms: zlib zstd
+gcc version 11.2.0 (Ubuntu 11.2.0-19ubuntu1) 
+COLLECT_GCC_OPTIONS='-E' '-v' '-o' '/dev/null' '-mtune=generic' '-march=x86-64'
+ /usr/lib/gcc/x86_64-linux-gnu/11/cc1 -E -quiet -v -imultiarch x86_64-linux-gnu /dev/null -o /dev/null -mtune=generic -march=x86-64 -fasynchronous-unwind-tables -fstack-protector-strong -Wformat -Wformat-security -fstack-clash-protection -fcf-protection -dumpbase null
+ignoring nonexistent directory "/usr/local/include/x86_64-linux-gnu"
+ignoring nonexistent directory "/usr/lib/gcc/x86_64-linux-gnu/11/include-fixed"
+ignoring nonexistent directory "/usr/lib/gcc/x86_64-linux-gnu/11/../../../../x86_64-linux-gnu/include"
+#include "..." search starts here:
+#include <...> search starts here:
+ /usr/lib/gcc/x86_64-linux-gnu/11/include
+ /usr/local/include
+ /usr/include/x86_64-linux-gnu
+ /usr/include
+End of search list.
+COMPILER_PATH=/usr/lib/gcc/x86_64-linux-gnu/11/:/usr/lib/gcc/x86_64-linux-gnu/11/:/usr/lib/gcc/x86_64-linux-gnu/:/usr/lib/gcc/x86_64-linux-gnu/11/:/usr/lib/gcc/x86_64-linux-gnu/
+LIBRARY_PATH=/usr/lib/gcc/x86_64-linux-gnu/11/:/usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/:/usr/lib/gcc/x86_64-linux-gnu/11/../../../../lib/:/lib/x86_64-linux-gnu/:/lib/../lib/:/usr/lib/x86_64-linux-gnu/:/usr/lib/../lib/:/usr/lib/gcc/x86_64-linux-gnu/11/../../../:/lib/:/usr/lib/
+COLLECT_GCC_OPTIONS='-E' '-v' '-o' '/dev/null' '-mtune=generic' '-march=x86-64'
+Juanfer MERCIER — Aujourd’hui à 10:52
+on a exactement le mm résultat pour cette commande
+Juanfer MERCIER — Aujourd’hui à 11:46
+j'ai modifié quelques petits trucs vite fait par rapport à l'an dernier mais au moins avec ce squelette suffit de modifier un peu la façon de lire les données, il fonctionne en ligne de commande le code (genre 
+java baseball didactic.txt
+)
+/**
+ * -------------------------------------------
+ *                  baseball.java
+ * -------------------------------------------
+ *
+ * Year : 2022
+ * Course : Graphs and networks
+ * Authors : Juanfer MERCIER, Adrien PICHON
+ **/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.System;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class Baseball {
+    /* ANSI escape codes to print text with a certain color
+     *
+     * DISCLAIMER : The following constants where borrowed from :
+     * https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+     *
+     * author   : WhiteFang34, shakram02
+     * editor   : SergeyB
+     */
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\033[1;31m";
+    public static final String ANSI_GREEN = "\033[1;32m";
+    public static final String ANSI_PURPLE = "\033[1;35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    /* Returns the extension of the filename
+     *
+     * author   : Juanfer MERCIER
+     * date     : last modified December 27th 2021
+     */
+    public static /*Optional<String>*/ String getExtension(String filename) {
+        if(filename.contains("."))
+            return filename.substring(filename.lastIndexOf(".") + 1);
+        else
+            return "";
+    }
+
+    /* Displays the program's manual */
+    private static void usage() {
+        System.err.println("Usage: java BinIm file.txt");
+        System.err.println("Solve the image binarization problem on instance file.txt");
+    }
+
+    /* Return next int from file when possible, -1 otherwise */
+    private static int getInt(Scanner file) {
+        if(file.hasNextInt())
+            return file.nextInt();
+        return -1;
+    }
+
+    /* Construct flow network using the data provided
+     *
+     * n    : number of rows
+     * m    : number of columns
+     * A    : foreground likelihood score
+     * B    : background likelihood score
+     * Ph   : horizontal penalties
+     * Pv   : vertical penalties
+     */
+    public static FlowNetwork ConstructionReseau(int n, int m, int[][] A, int[][] B, int[][] Ph, int[][] Pv) {
+        return new FlowNetwork(n, m, A, B, Ph, Pv);
+    }
+
+    /* Compute the network's maximum flow
+     *
+     * R    : the flow network
+     */
+    public static int CalculFlotMax(FlowNetwork R) {
+        return R.getMaxFlow();
+    }
+
+    /* Compute the network's minimum capacity cut
+     *
+     * R    : the flow network
+     */
+    public static boolean[][] CalculCoupeMin(FlowNetwork R) {
+        return R.getMinCut();
+    }
+
+    /* Print the network's minimum capacity cut sets
+     *
+     * R    : the flow network
+     */
+    public static void PrintMinCut(FlowNetwork R) {
+        boolean cut[][] = CalculCoupeMin(R);
+
+        for(boolean[] t : cut){
+            for(boolean b : t)
+                if(b) // Belongs to set B
+                    System.out.print(ANSI_PURPLE + "B" + ANSI_RESET);
+                else  // Belongs to set A
+                    System.out.print(ANSI_CYAN + "A" + ANSI_RESET);
+            System.out.println();
+... (92 lignes restantes)
+Réduire
+baseball.java
+7 Ko
+﻿
+/**
+ * -------------------------------------------
+ *                  baseball.java
+ * -------------------------------------------
+ *
+ * Year : 2022
+ * Course : Graphs and networks
+ * Authors : Juanfer MERCIER, Adrien PICHON
+ **/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.System;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class Baseball {
+    /* ANSI escape codes to print text with a certain color
+     *
+     * DISCLAIMER : The following constants where borrowed from :
+     * https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+     *
+     * author   : WhiteFang34, shakram02
+     * editor   : SergeyB
+     */
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\033[1;31m";
+    public static final String ANSI_GREEN = "\033[1;32m";
+    public static final String ANSI_PURPLE = "\033[1;35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    /* Returns the extension of the filename
+     *
+     * author   : Juanfer MERCIER
+     * date     : last modified December 27th 2021
+     */
+    public static /*Optional<String>*/ String getExtension(String filename) {
+        if(filename.contains("."))
+            return filename.substring(filename.lastIndexOf(".") + 1);
+        else
+            return "";
+    }
+
+    /* Displays the program's manual */
+    private static void usage() {
+        System.err.println("Usage: java BinIm file.txt");
+        System.err.println("Solve the image binarization problem on instance file.txt");
+    }
+
+    /* Return next int from file when possible, -1 otherwise */
+    private static int getInt(Scanner file) {
+        if(file.hasNextInt())
+            return file.nextInt();
+        return -1;
+    }
+
+    /* Construct flow network using the data provided
+     *
+     * n    : number of rows
+     * m    : number of columns
+     * A    : foreground likelihood score
+     * B    : background likelihood score
+     * Ph   : horizontal penalties
+     * Pv   : vertical penalties
+     */
+    public static FlowNetwork ConstructionReseau(int n, int m, int[][] A, int[][] B, int[][] Ph, int[][] Pv) {
+        return new FlowNetwork(n, m, A, B, Ph, Pv);
+    }
+
+    /* Compute the network's maximum flow
+     *
+     * R    : the flow network
+     */
+    public static int CalculFlotMax(FlowNetwork R) {
+        return R.getMaxFlow();
+    }
+
+    /* Compute the network's minimum capacity cut
+     *
+     * R    : the flow network
+     */
+    public static boolean[][] CalculCoupeMin(FlowNetwork R) {
+        return R.getMinCut();
+    }
+
+    /* Print the network's minimum capacity cut sets
+     *
+     * R    : the flow network
+     */
+    public static void PrintMinCut(FlowNetwork R) {
+        boolean cut[][] = CalculCoupeMin(R);
+
+        for(boolean[] t : cut){
+            for(boolean b : t)
+                if(b) // Belongs to set B
+                    System.out.print(ANSI_PURPLE + "B" + ANSI_RESET);
+                else  // Belongs to set A
+                    System.out.print(ANSI_CYAN + "A" + ANSI_RESET);
+            System.out.println();
+        }
+    }
+
+    /* Solve the BinIm problem on the data provided
+     *
+     * n    : number of rows
+     * m    : number of columns
+     * A    : foreground likelihood score
+     * B    : background likelihood score
+     * Ph   : horizontal penalties
+     * Pv   : vertical penalties
+     */
+    public static void ResoudreBinIm(int n, int m, int[][] A, int[][] B, int[][] Ph, int[][] Pv) {
+        // Construct the network from file
+        FlowNetwork R = ConstructionReseau(n, m, A, B, Ph, Pv);
+
+        // Compute the maximum flow of R
+        int maxFlow = CalculFlotMax(R);
+
+        // Print cut
+        PrintMinCut(R);
+
+        System.out.println("\n" + ANSI_GREEN + "Maximum flow : " + maxFlow + ANSI_RESET);
+    }
+
+    public static void main(String args[]) {
+        // Check if length of args array is
+        // greater than 0
+        if(args.length == 1) {
+            // Check if filename has extension .txt
+            String ext = getExtension(args[0]);
+
+            if("txt".equals(ext)){
+                // CHECKS PASSED. Now we read the file...
+                try {
+                    Scanner file = new Scanner(new File(args[0]));
+
+                    // Loop variables
+                    int i = 0, j = 0, k = 0;
+
+                    // Number of rows
+                    int n = getInt(file);
+                    // Number of columns
+                    int m = getInt(file);
+
+                    // Set A
+                    int[][] A = new int[n][m];
+                    // Set B
+                    int[][] B = new int[n][m];
+                    // Horizontal and vertical penalties
+                    int[][] Ph = new int[n][m-1];
+                    int[][] Pv = new int[n-1][m];
+
+                    // Read values of A and B from file
+                    for(k = 0; k < 2; k++)
+                        for(i = 0; i < n; i++)
+                            for(j = 0; j < m; j++)
+                                if(k == 0)
+                                    A[i][j] = getInt(file);
+                                else
+                                    B[i][j] = getInt(file);
+
+                    // Read values of Ph from file
+                    for(i = 0; i < n; i++)
+                        for(j = 0; j < m-1; j++)
+                            Ph[i][j] = getInt(file);
+
+                    // Read values of Pv from file
+                    for(i = 0; i < n-1; i++)
+                        for(j = 0; j < m; j++)
+                            Pv[i][j] = getInt(file);
+
+                    // Solve BinIm problem
+                    ResoudreBinIm(n, m, A, B, Ph, Pv);
+
+                    // Close input stream
+                    file.close();
+                } catch(FileNotFoundException e) { // File not found
+                    System.err.println(ANSI_RED + "File \"" + args[0] + "\" not found\n" + ANSI_RESET);
+                    usage();
+                }
+            } else { // File extension not ".txt"
+                System.err.println(ANSI_RED + "File extension shall be \".txt\" (without quotes)\n" + ANSI_RESET);
+                usage();
+            }
+        } else { // No file specified
+            System.err.println(ANSI_RED + "Filename not specified\n" + ANSI_RESET);
+            usage();
+        }
     }
 }
+baseball.java
+7 Ko
