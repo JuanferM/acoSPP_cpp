564b4009d135ee5fa58287c55f5f57cf1f6cb38e
Juanfer014 <mercier.juanfer@gmail.com>
2023-07-05 22:57:20 +0200

    smart pointers

--- include/plots.hpp
+++ include/plots.hpp
@@ -4,6 +4,7 @@
 #include "librarySPP.hpp"
 
 #include <cmath>
+#include <string>
 // Modified version of https://github.com/alandefreitas/matplotplusplus
 #include <matplot/matplot.h>
 #include <matplot/util/common.h>
@@ -11,49 +12,49 @@
 // Plot l'examen d'un run de ACO sur
 // une instance
 void plotRunACO(
-        const std::string instance,
-        const std::vector<int>& zInits,
-        const std::vector<int>& zAmels,
-        const std::vector<int>& zBests,
-        const std::vector<float>& probas,
-        const int zinit,
-        const int zls,
-        const int zbest,
-        const int done_iter,
-        std::string save_path = "",
-        bool silent_mode = false);
+    const std::string instance,
+    const std::vector<int>& zInits,
+    const std::vector<int>& zAmels,
+    const std::vector<int>& zBests,
+    const std::vector<float>& probas,
+    const int zinit,
+    const int zls,
+    const int zbest,
+    const int done_iter,
+    std::string save_path = "",
+    bool silent_mode = false);
 
 // Plot des niveaux de phéromones avant
 // et après le premier restart
 void plotPhiRunACO(
-        const std::string instance,
-        const int n,
-        const float* phi,
-        bool before = true,
-        std::string save_path = "",
-        bool silent_mode = false);
+    const std::string instance,
+    const int n,
+    const float* phi,
+    bool before = true,
+    std::string save_path = "",
+    bool silent_mode = false);
 
 // Plot le bilan de tous les runs de ACO
 // sur une instance (plot exactement NUM_DIVISION
 // points avec NUM_DIVISION <= NUM_ITER)
 void plotAnalyseACO(
-        const std::string instance,
-        const std::vector<double>& divs,
-        const std::vector<int>& zMin,
-        const std::vector<double>& zMoy,
-        const std::vector<int>& zMax,
-        const int allrunzmin,
-        const float allrunzmoy,
-        const int allrunzmax,
-        std::string save_path = "",
-        bool silent_mode = false);
+    const std::string instance,
+    const std::vector<double>& divs,
+    const std::vector<int>& zMin,
+    const std::vector<double>& zMoy,
+    const std::vector<int>& zMax,
+    const int allrunzmin,
+    const float allrunzmoy,
+    const int allrunzmax,
+    std::string save_path = "",
+    bool silent_mode = false);
 
 // Plot le bilan CPUt pour chaque instance (le
 // temps d'exécution moyen d'un run)
 void plotCPUt(
-        std::vector<std::string>& fnames,
-        std::vector<float>& tMoy,
-        std::string save_path = "",
-        bool silent_mode = false);
+    std::vector<std::string>& fnames,
+    std::vector<float>& tMoy,
+    std::string save_path = "",
+    bool silent_mode = false);
 
 #endif /* end of include guard: PLOTS_H */
--- src/main.cpp
+++ src/main.cpp
@@ -4,197 +4,198 @@
 #include <omp.h>
 
 // Paramètres GLPK
-#define USE_GLPK        false
-#define VERBOSE_GLPK    false
+#define USE_GLPK    false
+#define VERBOSE_GLPK  false
 
 // Paramètres OpenMP
-#define PARALLEL        true
-#define MAX_THREADS     10
+#define PARALLEL      true
+#define MAX_THREADS   10
 
 // Paramètres ACO
-#define NUM_RUN         10
-#define MAX_ANTS        15
-#define MAX_ITER        200
-#define PHI_INIT        1.0
-#define RHO_E           0.8
-#define PHI_NUL         0.001
-#define ITER_STAGNANT   8
-#define DEEPSEARCH      false
-#define RESTARTSTOP     true
-#define MAX_RESTART     2
+#define NUM_RUN       1
+#define MAX_ANTS      15
+#define MAX_ITER      200
+#define PHI_INIT      1.0
+#define RHO_E         0.8
+#define PHI_NUL       0.001
+#define ITER_STAGNANT 8
+#define DEEPSEARCH    false
+#define RESTARTSTOP   true
+#define MAX_RESTART   2
 
 // Paramètres plots
-#define CAPTURE_PHI     true
-#define INTERACTIVE     false
-#define SILENT_MODE     true
-#define PATH_PLOT       "exp/"
-#define NUM_DIVISION    20
+#define CAPTURE_PHI   true
+#define INTERACTIVE   false
+#define SILENT_MODE   false
+#define PATH_PLOT     ""
+#define NUM_DIVISION  20
 
 int main() {
-    // This program will create different sequence of
-    // random numbers on every program run
-    // Use current time as seed for random generator
-    std::string pwd(std::filesystem::current_path());
-    std::string path(pwd + "/instances/");
-    std::cout.precision(3);
+  // This program will create different sequence of
+  // random numbers on every program run
+  // Use current time as seed for random generator
+  std::string pwd(std::filesystem::current_path());
+  std::string path(pwd + "/../instances/");
+  std::cout.precision(3);
 
-    m_print(std::cout, _CLRd, "Etudiants : MERCIER et PICHON\n", _CLR);
-    #if !USE_GLPK
-        #if MAX_ITER < 2 // We need at least two iterations or else the plots
-            #undef MAX_ITER //break
-            #define MAX_ITER 2
-        #endif
-        #if NUM_RUN < 1
-            #undef NUM_RUN
-            #define NUM_RUN 1
-        #endif
-        #if MAX_THREADS < 1
-            #undef MAX_THREADS
-            #define MAX_THREADS 10
-        #endif
-        #if ITER_STAGNANT > MAX_ITER
-            #undef ITER_STAGNANT
-            #define ITER_STAGNANT int(MAX_ITER/10)
-        #endif
-        #if MAX_ANTS < 1
-            #undef MAX_ANTS
-            #define MAX_ANTS 1
-        #endif
-
-        INIT_TIMER(); srand(time(NULL));
-        if(PARALLEL) omp_set_num_threads(MAX_THREADS);
-
-        const int _NBD_ = NUM_DIVISION > MAX_ITER ? MAX_ITER : NUM_DIVISION;
-        const float RHO_D = PHI_INIT * (1.0 - RHO_E);
-        m_print(std::cout, _CLP, "\nnombre de runs\t\t: ", NUM_RUN);
-        m_print(std::cout, "\nnombre de fourmis\t: ", MAX_ANTS);
-        m_print(std::cout, "\nnombre d'itérations\t: ", MAX_ITER);
-        m_print(std::cout, "\nvaleur initiale ϕ\t: ", PHI_INIT);
-        m_print(std::cout, "\ntaux d'évaporation\t: ", RHO_E);
-        m_print(std::cout, "\ntaux de dépôt\t\t: ", RHO_D);
-        m_print(std::cout, "\nseuil phéromone nul\t: ", PHI_NUL);
-        m_print(std::cout, "\nmax. cycles stagnants\t: ", ITER_STAGNANT);
-        m_print(std::cout, "\nparallélisation\t\t: ", (PARALLEL ? "oui" : "non"));
-        if(RESTARTSTOP)
-            m_print(std::cout, "\narrêt au restart #" + std::to_string(MAX_RESTART) + "\t: oui");
-        if(PARALLEL)
-            m_print(std::cout, "\nnombre de threads\t: ", MAX_THREADS);
-        m_print(std::cout, "\ndescente profonde\t: ", (DEEPSEARCH ? "oui" : "non"));
-        m_print(std::cout, "\nplot des runs en \t: ", _NBD_, " points");
-        if(std::string("").compare(PATH_PLOT))
-            m_print(std::cout, "\nrépertoire plots \t: ", PATH_PLOT);
-        m_print(std::cout, "\ncapturer ϕ\t\t: ", (CAPTURE_PHI ? "oui" : "non"));
-        m_print(std::cout, "\nmode silencieux\t\t: ", (SILENT_MODE ? "oui" : "non"));
-        m_print(std::cout, "\nmode intéractif\t\t: ", (INTERACTIVE ? "oui" : "non"), "\n\n", _CLR);
-
-        char *A(nullptr);
-        float t(0), *U(nullptr), *phi(nullptr), *phi_bef(nullptr), *phi_aft(nullptr);
-        int ins(0), run(0), div(0), zinit(-1), zls(-1), zbest(-1),
-            done_iter(0), m(-1), n(-1), *C(nullptr);
-        // Vecteur de phéromones
-        std::vector<int> zInits(MAX_ITER * MAX_ANTS, 0),
-                         zAmels(MAX_ITER * MAX_ANTS, 0),
-                         zBests(MAX_ITER * MAX_ANTS, 0);
-        std::vector<float> probas(MAX_ITER * MAX_ANTS, 0.0);
-        std::vector<float> tMoy;
-        std::vector<double> divs;
-    #else
-        float tt(0.f);;
-        m_print(std::cout, _CLP, "\nRÉSOLUTION AVEC GLPK\n\n", _CLR);
+  m_print(std::cout, _CLRd, "Etudiants : MERCIER et PICHON\n", _CLR);
+  #if !USE_GLPK
+    #if MAX_ITER < 2 // We need at least two iterations or else the plots
+      #undef MAX_ITER //break
+      #define MAX_ITER 2
+    #endif
+    #if NUM_RUN < 1
+      #undef NUM_RUN
+      #define NUM_RUN 1
+    #endif
+    #if MAX_THREADS < 1
+      #undef MAX_THREADS
+      #define MAX_THREADS 10
+    #endif
+    #if ITER_STAGNANT > MAX_ITER
+      #undef ITER_STAGNANT
+      #define ITER_STAGNANT int(MAX_ITER/10)
+    #endif
+    #if MAX_ANTS < 1
+      #undef MAX_ANTS
+      #define MAX_ANTS 1
     #endif
 
-    std::vector<std::string> fnames = getfname(path);
-    for(auto instance : fnames) {
-        #if USE_GLPK
-            modelSPP(instance, path, &tt, VERBOSE_GLPK);
-        #else
-            float allrunzmoy(0);
-            int allrunzmin(INT_MAX), allrunzmax(INT_MIN);
-            std::vector<int>    zMin(_NBD_, INT_MAX),
-                                zMax(_NBD_, INT_MIN);
-            std::vector<double> zMoy(_NBD_, 0);
-            if(tMoy.size() == 0) {
-                for(ins = 0; ins < (int)fnames.size(); ins++)
-                    tMoy.push_back(0);
-                ins = 0;
-            }
+    INIT_TIMER(); srand(time(NULL));
+    if(PARALLEL) omp_set_num_threads(MAX_THREADS);
 
-            // Load one numerical instance (also init phi)
-            std::tie(m, n, C, A, U, phi) = loadSPP(path + instance, PHI_INIT);
-            m_print(std::cout, _CLB, "\nInstance : ", instance, "\n", _CLR);
+    const int _NBD_ = NUM_DIVISION > MAX_ITER ? MAX_ITER : NUM_DIVISION;
+    const float RHO_D = PHI_INIT * (1.0 - RHO_E);
+    m_print(std::cout, _CLP, "\nnombre de runs\t\t: ", NUM_RUN);
+    m_print(std::cout, "\nnombre de fourmis\t: ", MAX_ANTS);
+    m_print(std::cout, "\nnombre d'itérations\t: ", MAX_ITER);
+    m_print(std::cout, "\nvaleur initiale ϕ\t: ", PHI_INIT);
+    m_print(std::cout, "\ntaux d'évaporation\t: ", RHO_E);
+    m_print(std::cout, "\ntaux de dépôt\t\t: ", RHO_D);
+    m_print(std::cout, "\nseuil phéromone nul\t: ", PHI_NUL);
+    m_print(std::cout, "\nmax. cycles stagnants\t: ", ITER_STAGNANT);
+    m_print(std::cout, "\nparallélisation\t\t: ", (PARALLEL ? "oui" : "non"));
+    if(RESTARTSTOP)
+      m_print(std::cout, "\narrêt au restart #" + std::to_string(MAX_RESTART) + "\t: oui");
+    if(PARALLEL)
+      m_print(std::cout, "\nnombre de threads\t: ", MAX_THREADS);
+    m_print(std::cout, "\ndescente profonde\t: ", (DEEPSEARCH ? "oui" : "non"));
+    m_print(std::cout, "\nplot des runs en \t: ", _NBD_, " points");
+    if(std::string("").compare(PATH_PLOT))
+      m_print(std::cout, "\nrépertoire plots \t: ", PATH_PLOT);
+    m_print(std::cout, "\ncapturer ϕ\t\t: ", (CAPTURE_PHI ? "oui" : "non"));
+    m_print(std::cout, "\nmode silencieux\t\t: ", (SILENT_MODE ? "oui" : "non"));
+    m_print(std::cout, "\nmode intéractif\t\t: ", (INTERACTIVE ? "oui" : "non"), "\n\n", _CLR);
 
-            m_print(std::cout, "Run exécutés :");
-            for(run = 0; run < NUM_RUN; run++) {
-                // Run ACO NUM_RUN times
-                TIMED(t,
-                    std::tie(zinit, zls, zbest, done_iter) = ACO(m, n, C, A, U,
-                        zInits, zAmels, zBests, probas, phi, &phi_bef, &phi_aft,
-                        PHI_NUL, RHO_E, RHO_D, ITER_STAGNANT, MAX_ANTS, MAX_ITER,
-                        DEEPSEARCH, PARALLEL, RESTARTSTOP, MAX_RESTART, CAPTURE_PHI);
-                );
-                tMoy[ins] = (!run) ? t : tMoy[ins]+t;
-                // Compute zMax, zMin and zMoy NUM_DIVISION time
-                divs = matplot::transform(
-                    matplot::linspace(1, done_iter/(float)MAX_ANTS, _NBD_),
-                    [](double x) {return (int)x;});
-                if(MAX_ITER-1 <= 1) divs[0] = 1;
-                int idx = 0;
-                for(div = 0; div < _NBD_; div++) {
-                    idx = (divs[div]-1)*MAX_ANTS;
-                    zMin[div] = std::min(zBests[idx], zMin[div]);
-                    zMax[div] = std::max(zBests[idx], zMax[div]);
-                    zMoy[div] += zBests[idx];
-                }
-                // Compute allrunzmin, allrunzmoy and allrunzmax
-                allrunzmin = std::min(allrunzmin, zbest);
-                allrunzmax = std::max(allrunzmax, zbest);
-                allrunzmoy += zbest;
+    float t(0), *phi_bef(nullptr), *phi_aft(nullptr);
+    int ins(0), run(0), div(0), zinit(-1), zls(-1), zbest(-1),
+      done_iter(0), m(-1), n(-1);
+    std::unique_ptr<int[]> C;
+    std::unique_ptr<char[]> A;
+    std::unique_ptr<float[]> U;
+    std::unique_ptr<float[]> phi;
+    // Vecteur de phéromones
+    std::vector<int> zInits(MAX_ITER * MAX_ANTS, 0),
+             zAmels(MAX_ITER * MAX_ANTS, 0),
+             zBests(MAX_ITER * MAX_ANTS, 0);
+    std::vector<float> probas(MAX_ITER * MAX_ANTS, 0.0);
+    std::vector<float> tMoy;
+    std::vector<double> divs;
+  #else
+    float tt(0.f);;
+    m_print(std::cout, _CLP, "\nRÉSOLUTION AVEC GLPK\n\n", _CLR);
+  #endif
 
-                m_print(std::cout, " ", run+1);
-            }
-
-            // Finish computing average z values
-            allrunzmoy /= (double)NUM_RUN;
-            for(div = 0; div < _NBD_; div++) zMoy[div] /= (double)NUM_RUN;
-
-            // Plots
-            m_print(std::cout, "\nPlot du dernier run...\n");
-            plotRunACO(instance, zInits, zAmels, zBests, probas, zinit,
-                        zls, zbest, done_iter, PATH_PLOT, SILENT_MODE);
-            if(CAPTURE_PHI && phi_bef && phi_aft) {
-                m_print(std::cout, "Plot des niveaux de phéromones avant le premier restart...\n");
-                plotPhiRunACO(instance, n, phi_bef, true, PATH_PLOT, SILENT_MODE);
-                m_print(std::cout, "Plot des niveaux de phéromones après le premier restart...\n");
-                plotPhiRunACO(instance, n, phi_aft, false, PATH_PLOT, SILENT_MODE);
-                if(phi_bef) delete[] phi_bef, phi_bef = nullptr;
-                if(phi_aft) delete[] phi_aft, phi_aft = nullptr;
-            }
-            m_print(std::cout, "Bilan de l'ensemble des runs...\n");
-            plotAnalyseACO(instance, divs, zMin, zMoy, zMax, allrunzmin,
-                    allrunzmoy, allrunzmax, PATH_PLOT, SILENT_MODE);
-
-            /* MOST IMPORTANT SECTIONS */
-            freeSPP(C, A, U, phi);
-            ins++;
-        #endif
-    }
-
+  std::vector<std::string> fnames = getfname(path);
+  for(auto instance : fnames) {
     #if USE_GLPK
-        glp_free_env();
+      modelSPP(instance, path, &tt, VERBOSE_GLPK);
     #else
-        // Finish computing average CPUt values
+      float allrunzmoy(0);
+      int allrunzmin(INT_MAX), allrunzmax(INT_MIN);
+      std::vector<int>  zMin(_NBD_, INT_MAX),
+                zMax(_NBD_, INT_MIN);
+      std::vector<double> zMoy(_NBD_, 0);
+      if(tMoy.size() == 0) {
         for(ins = 0; ins < (int)fnames.size(); ins++)
-            tMoy[ins] /= NUM_RUN;
+          tMoy.push_back(0);
+        ins = 0;
+      }
 
-        // Plots
-        m_print(std::cout, "\n\nBilan CPUt moyen (par run) pour chaque instance...\n");
-        plotCPUt(fnames, tMoy, PATH_PLOT, SILENT_MODE);
+      // Load one numerical instance (also init phi)
+      std::tie(m, n, C, A, U, phi) = loadSPP(path + instance, PHI_INIT);
+      m_print(std::cout, _CLB, "\nInstance : ", instance, "\n", _CLR);
 
-        if(INTERACTIVE) {
-            m_print(std::cout, _CLG, "\nMODE INTÉRACTIF: Appuyez sur ENTRER pour terminer...\n", _CLR);
-            std::cin.get();
+      m_print(std::cout, "Run exécutés :");
+      for(run = 0; run < NUM_RUN; run++) {
+        // Run ACO NUM_RUN times
+        TIMED(t,
+          std::tie(zinit, zls, zbest, done_iter) = ACO(m, n, C.get(), A.get(),
+            U.get(), zInits, zAmels, zBests, probas, phi.get(), &phi_bef, &phi_aft,
+            PHI_NUL, RHO_E, RHO_D, ITER_STAGNANT, MAX_ANTS, MAX_ITER,
+            DEEPSEARCH, PARALLEL, RESTARTSTOP, MAX_RESTART, CAPTURE_PHI);
+        );
+        tMoy[ins] = (!run) ? t : tMoy[ins]+t;
+        // Compute zMax, zMin and zMoy NUM_DIVISION time
+        divs = matplot::transform(
+          matplot::linspace(1, done_iter/(float)MAX_ANTS, _NBD_),
+          [](double x) {return (int)x;});
+        if(MAX_ITER-1 <= 1) divs[0] = 1;
+        int idx = 0;
+        for(div = 0; div < _NBD_; div++) {
+          idx = (divs[div]-1)*MAX_ANTS;
+          zMin[div] = std::min(zBests[idx], zMin[div]);
+          zMax[div] = std::max(zBests[idx], zMax[div]);
+          zMoy[div] += zBests[idx];
         }
-    #endif
+        // Compute allrunzmin, allrunzmoy and allrunzmax
+        allrunzmin = std::min(allrunzmin, zbest);
+        allrunzmax = std::max(allrunzmax, zbest);
+        allrunzmoy += zbest;
 
-    return 0;
+        m_print(std::cout, " ", run+1);
+      }
+
+      // Finish computing average z values
+      allrunzmoy /= (double)NUM_RUN;
+      for(div = 0; div < _NBD_; div++) zMoy[div] /= (double)NUM_RUN;
+
+      // Plots
+      m_print(std::cout, "\nPlot du dernier run...\n");
+      plotRunACO(instance, zInits, zAmels, zBests, probas, zinit,
+            zls, zbest, done_iter, PATH_PLOT, SILENT_MODE);
+      if(CAPTURE_PHI && phi_bef && phi_aft) {
+        m_print(std::cout, "Plot des niveaux de phéromones avant le premier restart...\n");
+        plotPhiRunACO(instance, n, phi_bef, true, PATH_PLOT, SILENT_MODE);
+        m_print(std::cout, "Plot des niveaux de phéromones après le premier restart...\n");
+        plotPhiRunACO(instance, n, phi_aft, false, PATH_PLOT, SILENT_MODE);
+        if(phi_bef) delete[] phi_bef, phi_bef = nullptr;
+        if(phi_aft) delete[] phi_aft, phi_aft = nullptr;
+      }
+      m_print(std::cout, "Bilan de l'ensemble des runs...\n");
+      plotAnalyseACO(instance, divs, zMin, zMoy, zMax, allrunzmin,
+          allrunzmoy, allrunzmax, PATH_PLOT, SILENT_MODE);
+
+      ins++;
+    #endif
+  }
+
+  #if USE_GLPK
+    glp_free_env();
+  #else
+    // Finish computing average CPUt values
+    for(ins = 0; ins < (int)fnames.size(); ins++)
+      tMoy[ins] /= NUM_RUN;
+
+    // Plots
+    m_print(std::cout, "\n\nBilan CPUt moyen (par run) pour chaque instance...\n");
+    plotCPUt(fnames, tMoy, PATH_PLOT, SILENT_MODE);
+
+    if(INTERACTIVE) {
+      m_print(std::cout, _CLG, "\nMODE INTÉRACTIF: Appuyez sur ENTRER pour terminer...\n", _CLR);
+      std::cin.get();
+    }
+  #endif
+
+  return 0;
 }
